d9b5a8e4e102e52c90dbc58bc3419a735d969504
sherzach <goblix9@gmail.com>
2020-10-21 21:56:27 +0100

    Added in accuracy calculation

--- CNN.py
+++ CNN.py
@@ -46,14 +46,15 @@ class CNN(torch.nn.Module):
     def __init__(self):
         super().__init__()
         self.cv_layers = torch.nn.Sequential(
-            torch.nn.Conv2d(1, 10, kernel_size=3), # 1 x 28 x 28
-            torch.nn.ReLU(),                        # 10 x 26 x 26
-            torch.nn.Conv2d(10, 20, kernel_size=3), # 20 x 24 x 24
+            torch.nn.Conv2d(1, 16, kernel_size=5), # 1 x 28 x 28
+            torch.nn.ReLU(),                        # 16 x 24 x 24
+            torch.nn.Conv2d(16, 32, kernel_size=5), # 32 x 20 x 20
             torch.nn.ReLU(),
+            # torch.nn.MaxPool2d(2), # 20 x 12 x 12
             torch.nn.Flatten()
         )
         self.fc_layer = torch.nn.Sequential(
-            torch.nn.Linear(11520, 10)
+            torch.nn.Linear(32 * 20 * 20, 10)
         )
 
     def forward(self, x):
@@ -77,7 +78,7 @@ criterion = torch.nn.CrossEntropyLoss()
 writer = SummaryWriter(log_dir="runs")  
 
 # Train the model
-def train(model, epochs, k_size):
+def train(model, epochs):
     losses = []
     for epoch in range(epochs):
         for idx, batch in enumerate(train_loader):
@@ -93,8 +94,22 @@ def train(model, epochs, k_size):
     
     return(losses) 
 
+def accuracy(model, dataloader):
+    for inputs, labels in dataloader:
+        num_correct = 0
+        num_examples = len(test_data)                       # test DATA not test LOADER
+        for inputs, labels in test_loader:                  # for all exampls, over all mini-batches in the test dataset
+            predictions = model(inputs)
+            predictions = torch.max(predictions, axis=1)    # reduce to find max indices along direction which column varies
+            predictions = predictions[1]                    # torch.max returns (values, indices)
+            num_correct += int(sum(predictions == labels))
+        percent_correct = num_correct / num_examples * 100
+        print('Accuracy:', percent_correct)
+
+
 if __name__ == '__main__':
     # Piece our CNN flow here
-    train(cnn, 10, 3)
-
+    train(cnn, 1)
+    torch.save(cnn, "trained_model.pt")
+    
     
